15dbe013e3190ebb68ec67c710c64956356f7866
Elmaida Khoirotuzzuhria <56856908+Elmaida@users.noreply.github.com>
2020-01-13 14:39:46 -0800

    Java_Date
    program java ini berisi tentang contoh penggunaan dari java date baik itu untuk memformat tanggal dan waktu saat ini, menampilkan tanggal saat ini, menampikan waktu saat ini, dan juga menampilkan tanggal dan waktu saat ini secra bersama-sama.

---
+++ Date/src/Date_Time/MyClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Date_Time;
+
+import java.time.LocalDateTime;
+
+public class MyClass {
+    public static void main(String[] args) {
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println(myObj);
+    }
+}
